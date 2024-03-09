cd389391a687a013f27e2544efa195bac158d84e
ch <chlgur13@kakao.com>
2024-03-09 14:23:20 +0900

    이전의 가상화폐 가격 불러오기

--- temp.js
+++
@@ -1,9 +0,0 @@
-let coin = "btc";
-
-fetch(
-  "https://min-api.cryptocompare.com/data/price?fsym=" +
-    `${coin}` +
-    "&tsyms=USD"
-)
-  .then((r) => r.json())
-  .then((j) => console.log(j));
