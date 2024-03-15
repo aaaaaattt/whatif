0773859b91fb317ce25390a9c17b10b2c1457ee3
ch <chlgur13@kakao.com>
2024-03-16 00:21:31 +0900

    fetch --> async/await

--- temp.js
+++ temp.js
@@ -1,37 +1,49 @@
 //btn클릭 시 date 날짜를 timestamp로 변환
 
-let timestamp = 0;
 const res = document.getElementById("authors");
 let day = document.getElementById("date");
-document.getElementById("btn").addEventListener("click", (e) => {
-  timestamp = parseInt(new Date(day.value).getTime() / 1000);
-  // console.log(timestamp);
-  fetch(
-    "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=KRW&ts=" +
-      `${timestamp}`
-  )
-    .then((r) => r.json())
-    .then((j) => {
-      //해당 날짜의 가격
-      const btcprice = Math.floor(j.BTC.KRW);
-      let li = document.createElement("li");
-      li.innerHTML = btcprice;
-      res.appendChild(li);
-      fetch(
-        "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=KRW"
-      )
-        .then((r) => r.json())
-        .then((j) => {
-          //현재 가격
-          let won = Math.floor(j.BTC.KRW);
-          console.log(won);
-          //수익률
-          let temp = Math.floor(((won - btcprice) / btcprice) * 100) / 100;
-          console.log(temp);
-          let result = temp; //예상 수익률
-          let input = document.getElementById("investment");
-          let result2 = input.value * (1 + result); //
-          input.value = result2;
-        });
-    });
+
+document.getElementById("btn").addEventListener("click", async () => {
+  try {
+    const timestamp = parseInt(new Date(day.value).getTime() / 1000);
+
+    //과거 가격
+    const btcResponse = await fetch(
+      "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=KRW&ts=" +
+        `${timestamp}`
+    );
+    //과거 가격 불러오기 Error 발생
+    if (!btcResponse.ok) {
+      throw new Error("Failed to fetch Bitcoin price");
+    }
+    const btcData = await btcResponse.json();
+    const btcprice = Math.floor(btcData.BTC.KRW);
+
+    //현재 가격
+    const currentPriceResponse = await fetch(
+      "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=KRW"
+    );
+    //현재 가격 불러오기 Error 발생
+    if (!currentPriceResponse.ok) {
+      throw new Error("Failed to fetch current Bitcoin price");
+    }
+
+    const currentPriceData = await currentPriceResponse.json();
+    const currentPrice = Math.floor(currentPriceData.BTC.KRW);
+    const profitPercentage =
+      Math.floor(((currentPrice - btcprice) / btcprice) * 100) / 100;
+    const investmentInput = document.getElementById("investment");
+    const result = investmentInput.value * (1 + profitPercentage);
+
+    //이번 결과 초기화
+    while (res.firstChild) {
+      res.removeChild(res.firstChild);
+    }
+
+    const resultDiv = document.createElement("div");
+    resultDiv.innerText = result;
+    res.appendChild(resultDiv);
+  } catch (error) {
+    console.error("An error occured: ", error);
+  }
 });
