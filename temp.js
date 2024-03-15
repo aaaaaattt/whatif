bd2c5f78452a736cc48344677882b7cfd5312b63
ch <chlgur13@kakao.com>
2024-03-15 14:52:47 +0900

    예상 수익 계산

--- temp.js
+++ temp.js
@@ -5,13 +5,14 @@ const res = document.getElementById("authors");
 let day = document.getElementById("date");
 document.getElementById("btn").addEventListener("click", (e) => {
   timestamp = parseInt(new Date(day.value).getTime() / 1000);
-  console.log(timestamp);
+  // console.log(timestamp);
   fetch(
     "https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=KRW&ts=" +
       `${timestamp}`
   )
     .then((r) => r.json())
     .then((j) => {
+      //해당 날짜의 가격
       const btcprice = Math.floor(j.BTC.KRW);
       let li = document.createElement("li");
       li.innerHTML = btcprice;
@@ -21,9 +22,16 @@ document.getElementById("btn").addEventListener("click", (e) => {
       )
         .then((r) => r.json())
         .then((j) => {
+          //현재 가격
+          let won = Math.floor(j.BTC.KRW);
+          console.log(won);
+          //수익률
+          let temp = Math.floor(((won - btcprice) / btcprice) * 100) / 100;
+          console.log(temp);
+          let result = temp; //예상 수익률
           let input = document.getElementById("investment");
-          input.value;
+          let result2 = input.value * (1 + result); //
+          input.value = result2;
         });
     });
 });
-//순이익/투자 비용 x 100
