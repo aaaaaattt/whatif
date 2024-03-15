82fcf58dd7176b0c150049d8592ab5a055e8cda1
ch <chlgur13@kakao.com>
2024-03-16 02:39:35 +0900

    코인 목록 추가

---
+++ coinlist.js
@@ -0,0 +1,34 @@
+const apiKey =
+  "bd4f605df59891a7e298a7227117404b2215dba9008a7b6e24206b6aadd5b038";
+
+async function fetchCoinList() {
+  try {
+    const response = await fetch(
+      "https://min-api.cryptocompare.com/data/all/coinlist"
+    );
+    const data = await response.json();
+    return data.Data;
+  } catch (err) {
+    console.error("Error fetchng coin list:", error);
+    return null;
+  }
+}
+
+fetchCoinList().then((coinList) => {
+  if (coinList) {
+    let CoinList = Object.keys(coinList).slice(0, 100);
+
+    let selectElement = document.getElementById("here");
+
+    CoinList.forEach((coinName) => {
+      console.log(coinName, selectElement);
+      let optionElement = document.createElement("option");
+      optionElement.textContent = coinName;
+      selectElement.appendChild(optionElement);
+    });
+  } else {
+    console.log("Failed to fetch coin list.");
+  }
+});
+
+fetchCoinList();
